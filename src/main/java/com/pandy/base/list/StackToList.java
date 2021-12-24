affa5271000841cf2b323b30af76c87d0b152833
PandyYang <yangpandy@gmail.com>
2021-12-24 17:27:30 +0800

    使用两个栈实现队列

---
+++ src/main/java/com/pandy/base/list/StackToList.java
@@ -0,0 +1,28 @@
+package com.pandy.base.list;
+
+import java.util.Stack;
+
+/**
+ * @author Pandy
+ * @date 24/12/2021
+ */
+public class StackToList {
+
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public void push(int a) {
+        stack1.push(a);
+    }
+
+    public int pop() throws Exception {
+        if (stack1.size() == 0 && stack2.size() == 0)
+            throw new Exception("stack is empty");
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
