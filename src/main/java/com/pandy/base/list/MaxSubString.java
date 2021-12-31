f7df3c93429b6e8c926b85f5a77143322f669431
PandyYang <yangpandy@gmail.com>
2021-12-31 17:31:42 +0800

    无重复字符的最长子串

---
+++ src/main/java/com/pandy/base/list/MaxSubString.java
@@ -0,0 +1,30 @@
+package com.pandy.base.list;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Pandy
+ * @date 31/12/2021
+ * 无重复字符的最长子串 使用滑动窗口求解
+ * 使用set进行滑动窗口
+ */
+public class MaxSubString {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int res = 0;
+        int i = 0, j = 0;
+        int len = s.length();
+        while (i < len && j < len) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
+                j++;
+                res = Math.max(res, j - i);
+            } else {
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return res;
+    }
+}
