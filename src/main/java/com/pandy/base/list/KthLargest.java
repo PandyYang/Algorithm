e726c8d46ecd74481cf4e5213b032ccdb08cb65a
PandyYang <yangpandy@gmail.com>
2022-01-01 09:49:28 +0800

    Kth

---
+++ src/main/java/com/pandy/base/list/KthLargest.java
@@ -0,0 +1,35 @@
+package com.pandy.base.list;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author: Pandy
+ * @create: 2022/1/1
+ * 数组中第K个最大的元素
+ **/
+public class KthLargest {
+    public static int kth(int[] arr, int k) {
+
+        // 使用优先队列进行解决
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>((o1, o2) -> {
+            return o1 - o2;
+        });
+
+        for (int i : arr) {
+            if (priorityQueue.size() < k) {
+                priorityQueue.add(i);
+            } else if (priorityQueue.peek() < i) {
+                priorityQueue.poll();
+                priorityQueue.add(i);
+            }
+        }
+        return priorityQueue.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int kth = kth(arr, 2);
+        System.out.println("kth = " + kth);
+    }
+}
