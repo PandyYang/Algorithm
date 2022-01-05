52ef2b7de699a603eef1272f5e3e450d83028548
PandyYang <yangpandy@gmail.com>
2022-01-05 14:17:50 +0800

    求下一个排列

---
+++ src/main/java/com/pandy/base/array/NextPermutation.java
@@ -0,0 +1,39 @@
+package com.pandy.base.array;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Pandy
+ * @date 5/1/2022
+ * 求下一个排列
+ */
+public class NextPermutation {
+    //
+    public void next(int[] arr) {
+
+        int k = arr.length - 2;
+        while (k >= 0 && arr[k] > arr[k+1])
+            k--;
+        if (k < 0) {
+            Arrays.sort(arr);
+            return;
+        }
+        int i = k+2;
+        while (i < arr.length && arr[i] > arr[k])
+            i++;
+        // 找到降序的一个序列值
+        int temp = arr[k];
+        arr[k] = arr[k-1];
+        arr[k-1] = temp;
+
+        int start = k+1, end = arr.length - 1;
+        while (start < end) {
+            int reTemp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = reTemp;
+            start ++;
+            end --;
+        }
+    }
+}
