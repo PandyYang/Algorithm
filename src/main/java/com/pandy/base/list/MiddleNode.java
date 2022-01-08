4f2489101969bf8fb2431197d692824bbaceede3
PandyYang <yangpandy@gmail.com>
2022-01-09 00:33:38 +0800

    求链表的中间节点

---
+++ src/main/java/com/pandy/base/list/MiddleNode.java
@@ -0,0 +1,22 @@
+package com.pandy.base.list;
+
+import com.pandy.common.ListNode;
+
+/**
+ * @author: Pandy
+ * @create: 2022/1/9
+ * 求链表的中间节点
+ **/
+public class MiddleNode {
+    private static ListNode getMin(ListNode root) {
+        if (root == null) return null;
+        ListNode slow = root;
+        ListNode fast = root;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
