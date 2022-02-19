70ac8c9f3cc9167cf62f46ed30a98c737c005851
PandyYang <yangpandy@gmail.com>
2022-02-19 18:32:29 +0800

    快排的思想和实现

--- src/main/java/com/pandy/base/sort/FastSort.java
+++ src/main/java/com/pandy/base/sort/FastSort.java
@@ -3,43 +3,43 @@ package com.pandy.base.sort;
 /**
  * @Author Pandy
  * @Date 2021/7/23 0:10
+ * 算法思想：通过一趟排序将待排记录分隔成独立的两部分，
+ * 其中一部分比关键字小，后面一部分比关键字大，然后再对这前后的
+ * 两部分分别采用这种方式进行排序，通过递归的运算最终达到整个序列有序。
+ *
  */
 public class FastSort {
-   private static void quickSort(int[] src, int begin, int end) {
-       if (begin < end) {
-           int key = src[begin];
-           int i = begin;
-           int j = end;
-           while (i < j) {
-               while (i< j && src[j] > key) {
-                   j--;
-               }
-
-               if (i < j) {
-                   src[i] = src[j];
-                   i++;
-               }
-
-               while (i < j && src[i] < key) {
-                   i++;
-               }
-
-               if (i < j) {
-                   src[j] = src[i];
-                   j--;
-               }
-           }
-           src[i] = key;
-           quickSort(src, begin, i - 1);
-           quickSort(src, i+1, end);
+   private static void quickSort(int[] arr, int left, int right) {
+       if (left > right) {
+           return;
        }
-   }
 
-    public static void main(String[] args) {
-       int[] arr= new int[]{5,4,3,2,1};
-        quickSort(arr, 0, arr.length - 1);
-        for (int i : arr) {
-            System.out.println("i = " + i);
-        }
-    }
+       // base中存放基准数
+       int base = arr[left];
+       int i = left, j = right;
+       while (i != j) {
+           // 顺序很重要，先从右边开始往左找，直到找到比base值小的数
+           while (arr[j] >= base && i < j) {
+               j--;
+           }
+
+           // 再从左往右找，直到找到比base值大的数
+           while (arr[i] <= base && i < j) {
+               i++;
+           }
+
+           if (i < j) {
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+           }
+       }
+
+       // 将基准数放到中间的位置
+       arr[left] = arr[i];
+       arr[i] = base;
+
+       quickSort(arr, left, i - 1);
+       quickSort(arr, i + 1, right);
+   }
 }
