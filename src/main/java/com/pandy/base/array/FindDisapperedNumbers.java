cd3d403e19a169dbe15852608a18736b6b02727a
PandyYang <yangpandy@gmail.com>
2022-03-23 14:43:35 +0800

    寻找消失的数字

--- src/main/java/com/pandy/base/array/FindDisapperedNumbers.java
+++ src/main/java/com/pandy/base/array/FindDisapperedNumbers.java
@@ -8,30 +8,11 @@ import java.util.List;
  * @create: 2022/2/26
  * 找出数组中消失的数字
  *
- * 数字范围1 ---- n，而数组下标 0 ---- n-1 则可用数组原地标记数字是否出现过
+ * 数字范围1 - n，而数组下标 0 - n-1 则可用数组原地标记数字是否出现过
  **/
 public class FindDisapperedNumbers {
 
-    public static List<Integer> find(int... nums) {
-        List<Integer> res = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int index = Math.abs(nums[i]) - 1;
-            if (nums[index] > 0) {
-                nums[index] = -nums[index];
-                System.out.println(nums[index]);
-            }
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                res.add(i+1);
-            }
-        }
-        return res;
-    }
-
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
         int len = nums.length;
         int index = 0;
         while (index < len) {
@@ -40,10 +21,12 @@ public class FindDisapperedNumbers {
                 index++;
             } else {
                 int targetIndex = nums[index] - 1;
+                // 数字在对应索引位置上（交换位置之后）1 5 3 4 5 6
                 if (nums[targetIndex] == nums[index]) {
                     index++;
                     continue;
                 }
+                // 将数字交换至对应的索引上
                 int temp = nums[targetIndex];
                 nums[targetIndex] = nums[index];
                 nums[index] = temp;
@@ -60,11 +43,10 @@ public class FindDisapperedNumbers {
     }
 
     public static void main(String[] args) {
-        System.out.println("=====================");
-        int[] arr = new int[]{4,3,2,7,8,2,3,1};
-        List<Integer> integers = find(arr);
-        for (Integer integer : integers) {
-            System.out.println(integer);
+        int[] nums = new int[]{1,3,4,5,5,6};
+        List<Integer> integers2 = findDisappearedNumbers(nums);
+        for (Integer res : integers2) {
+            System.out.println("integer = " + res);
         }
     }
 }
