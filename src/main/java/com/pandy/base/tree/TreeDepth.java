09573c84d58c8ad0d5d2f6517fe0ca0c582cca3a
PandyYang <yangpandy@gmail.com>
2022-03-23 17:46:17 +0800

    使用队列求解二叉树的最大深度

--- src/main/java/com/pandy/base/tree/TreeDepth.java
+++ src/main/java/com/pandy/base/tree/TreeDepth.java
@@ -2,6 +2,9 @@ package com.pandy.base.tree;
 
 import com.pandy.common.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author: Pandy
  * @create: 2022/1/15
@@ -31,5 +34,39 @@ public class TreeDepth {
     private int maxDepth(TreeNode root) {
         if (root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public int maxDepth2(TreeNode root) {
+        if(root == null)
+        {
+            return 0;
+        }
+        // 队列
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int res = 0;
+        // 按层扩展
+        while(!queue.isEmpty())
+        {
+            // 拿出该层所有节点，并压入子节点
+            int size = queue.size();
+            while(size > 0)
+            {
+                TreeNode node = queue.poll();
+
+                if(node.left != null)
+                {
+                    queue.offer(node.left);
+                }
+                if(node.right != null)
+                {
+                    queue.offer(node.right);
+                }
+                size-=1;
+            }
+            // 统计层数
+            res +=1;
+        }
+        return res;
     }
 }
