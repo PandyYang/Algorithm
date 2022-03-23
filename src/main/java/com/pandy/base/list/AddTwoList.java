80f386a86f7593cd9a65c2110dd9090b17c136a5
PandyYang <yangpandy@gmail.com>
2022-03-23 14:43:06 +0800

    两个链表相加

--- src/main/java/com/pandy/base/list/AddTwoList.java
+++ src/main/java/com/pandy/base/list/AddTwoList.java
@@ -6,22 +6,40 @@ import com.pandy.common.ListNode;
  * @author Pandy
  * @date 28/12/2021
  */
-public class AddTwoNumbers {
+public class AddTwoList {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        // 临时节点
         ListNode dummyHead =new ListNode(0);
+
         ListNode p = l1, q = l2, curr = dummyHead;
+
+        // 进位
         int carry = 0;
+
         while (p != null || q != null) {
+
+            // 链表依次的值
             int x = (p != null) ? p.val : 0;
             int y = (q != null) ? q.val : 0;
+
+            // 当前总值  进位 + 当前值
             int sum = carry + x + y;
+
+            // 计算进位
             carry = sum / 10;
+
+            // 链表节点构造
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
+
+            // 链表指针后移
             if (p != null) p = p.next;
             if (q != null) q = q.next;
         }
+
         if (carry > 0) curr.next = new ListNode(carry);
+
         return dummyHead.next;
     }
 
