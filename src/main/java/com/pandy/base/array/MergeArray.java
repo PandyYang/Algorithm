2a46970a33bee8024fa89cb1c52f2dbbda55f7e3
PandyYang <yangpandy@gmail.com>
2022-03-23 17:32:07 +0800

    合并两个有序数组 测试用例

--- src/main/java/com/pandy/base/array/MergeArray.java
+++ src/main/java/com/pandy/base/array/MergeArray.java
@@ -3,9 +3,12 @@ package com.pandy.base.array;
 /**
  * @Author Pandy
  * @Date 2021/7/31 15:17
+ *
+ * 合并两个数组 假设A数组的长度大于m+n-1
+ * 从后往前对比 然后存入A即可，长度为m+n-1
  */
 public class MergeArray {
-    public void merge(int[] A, int m, int[] B, int n) {
+    public static void merge(int[] A, int m, int[] B, int n) {
 
         // 各自长度 以及总长度
         int i = m-1, j = n-1, index = m+n-1;
@@ -17,6 +20,15 @@ public class MergeArray {
 
         while (j >= 0) {
             A[index--] = B[j--];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arrA = new int[]{1,3,5,7,9,0,0,0,0,0};
+        int[] arrB = new int[]{2,4,6,8,10};
+        merge(arrA, 5, arrB, 5);
+        for (int i : arrA) {
+            System.out.println("i = " + i);
         }
     }
 }
