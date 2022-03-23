ab86b11e9a1d3efcae66c5afb4094da157319ccd
PandyYang <yangpandy@gmail.com>
2022-03-23 21:41:36 +0800

    打家劫舍动态规划

---
+++ src/main/java/com/pandy/base/dynamic/Rob.java
@@ -0,0 +1,44 @@
+package com.pandy.base.dynamic;
+
+/**
+ * @author: Pandy
+ * @create: 2022/3/23
+ *
+ * 打家劫舍
+ *
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+ * 影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+
+ *输入：[1,2,3,1]
+ * 输出：4
+ * 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+ * 偷窃到的最高金额 = 1 + 3 = 4 。
+ *
+ * dp 方程 dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+ *
+ **/
+public class Rob {
+
+    public static int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+
+        return dp[nums.length -1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,7,9,3,1};
+        int rob = rob(arr);
+        System.out.println("rob = " + rob);
+    }
+}
