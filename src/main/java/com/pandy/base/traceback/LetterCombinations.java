6e96061b45a1d336f5b015d6b015cda72bf1dd5b
PandyYang <yangpandy@gmail.com>
2022-03-24 21:43:32 +0800

    电话号的组合

---
+++ src/main/java/com/pandy/base/traceback/LetterCombinations.java
@@ -0,0 +1,42 @@
+package com.pandy.base.traceback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Pandy
+ * @create: 2022/3/24
+ **/
+public class LetterCombinations {
+
+    public static List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        String[] str = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        for (int i = 0; i < digits.length(); i++) {
+            result = combination(result, str[digits.charAt(i) - '0' - 2]);
+        }
+        return result;
+    }
+
+    public static List<String> combination(List<String> s1, String s2) {
+        List<String> str = new ArrayList<>();
+        for (int i = 0; i < s2.length(); i++) {
+            if (s1.isEmpty()) {
+                str.add(s2.substring(i, i+1));
+            } else {
+                for (String s: s1) {
+                    str.add(s+s2.substring(i, i+1));
+                }
+            }
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        List<String> strings = letterCombinations("33");
+        for (String string : strings) {
+            System.out.println("string = " + string);
+        }
+    }
+}
