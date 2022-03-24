d8c0fd3ce4083e9ddaef1f05415e6fb14f379f39
PandyYang <yangpandy@gmail.com>
2022-03-24 20:56:38 +0800

    三数之和

--- src/main/java/com/pandy/base/array/SumOfThreeNum.java
+++ src/main/java/com/pandy/base/array/SumOfThreeNum.java
@@ -6,6 +6,13 @@ import java.util.*;
  * @author: Pandy
  * @create: 2022/1/1
  * 三数之和为0
+ *
+ *
+ *
+ * 采用分治的思想找出三个数相加等于 0，我们可以数组依次遍历，
+ * 每一项 a[i]我们都认为它是最终能够用组成 0 中的一个数字，
+ * 那么我们的目标就是找到剩下的元素（除 a[i]）两个相加等于-a[i].
+ *
  **/
 public class SumOfThreeNum {
     public List<List<Integer>> sum(int[] arr) {
@@ -15,11 +22,8 @@ public class SumOfThreeNum {
         for (int i = 0; i < arr.length; i++) {
             // 排序之后第一个数字大于0 则不可能有三数之和大于0
             if (arr[i] > 0) break;
-            // 将target作为arr[i] 则x + y = arr[i] 符合条件
-            // arr[i] = -target
-            // x + y + target = 0
-            // 此时的x y i 满足条件
-            int target = -arr[i];
+
+            int target = - arr[i];
             // 此时的   i i+1 arr.length - 1
             // 这三个位置分别对应 0 1 最后
             int left = i + 1;
