e4e0730e53d7b53b884e4a7bfe40048a8d98fde8
PandyYang <yangpandy@gmail.com>
2022-03-26 05:50:24 +0800

    数字的范围

---
+++ src/main/java/com/pandy/base/array/SearchRange.java
@@ -0,0 +1,60 @@
+package com.pandy.base.array;
+
+/**
+ * @author: Pandy
+ * @create: 2022/3/26
+ *
+ * 数字的范围 找出第一个和最后一个数字
+ **/
+public class SearchRange {
+
+    public static int[] searchRange(int[] nums, int target) {
+
+        int[] res = new int[]{-1, -1};
+        if (nums.length < 1) return res;
+        boolean flag = false;
+
+        if (nums.length == 1 && target != nums[0]){
+            return res;
+        }
+
+        if (nums.length == 1) return new int[] {0, 0};
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                res[0] = i;
+                flag = true;
+            }
+
+            if (i == nums.length - 1) {
+                flag = false;
+            }
+
+            if (flag) {
+                for (int j = i+1; j < nums.length; j++) {
+                    if (nums[j] == target) {
+                        res[1] = j;
+                        if (j == nums.length - 1) {
+                            return res;
+                        }
+                    } else {
+                        if (res[1] > res[0]) return res;
+                        flag = false;
+                    }
+                }
+            }
+        }
+        if (!flag) {
+            res[1] = res[0];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int []arr = new int[]{5,7,7,8,8,10};
+        int[] ints = searchRange(arr, 8);
+        for (int anInt : ints) {
+            System.out.println("anInt = " + anInt);
+        }
+    }
+}
