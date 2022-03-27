67f8159a4b948999cd7a1890b0645b43530c17a7
PandyYang <yangpandy@gmail.com>
2022-03-27 21:27:28 +0800

    求子集 动态对话 回溯 普通方法均可

---
+++ src/main/java/com/pandy/base/traceback/SubSets.java
@@ -0,0 +1,49 @@
+package com.pandy.base.traceback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Pandy
+ * @create: 2022/3/27
+ *
+ * 求子集
+ **/
+public class SubSets {
+
+    List<Integer> t = new ArrayList<>();
+    public List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> subSets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<>(t));
+            return;
+        }
+
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs(cur+1, nums);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+
+
+    public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int all = res.size();
+            for (int j = 0; j < all; j++) {
+                ArrayList<Integer> tmp = new ArrayList<>(res.get(j));
+                tmp.add(nums[i]);
+                res.add(tmp);
+            }
+        }
+        return res;
+    }
+}
