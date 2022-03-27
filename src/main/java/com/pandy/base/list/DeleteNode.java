152cc02f110ca3d3daaa6027e7af3da69944a8d5
PandyYang <yangpandy@gmail.com>
2022-03-27 23:10:30 +0800

    删除节点

--- src/main/java/com/pandy/base/list/DeleteNode.java
+++ src/main/java/com/pandy/base/list/DeleteNode.java
@@ -32,4 +32,18 @@ public class DeleteNode {
             }
         }
     }
+
+    public ListNode deleteNode(ListNode head, int val) {
+        if(head == null) return head;
+        ListNode cur = head;
+        ListNode pre = null;
+        if(cur.val == val) return head.next;
+        while(cur.val != val) {
+            pre= cur;
+            cur = cur.next;
+        }
+
+        pre.next = pre.next.next;
+        return head;
+    }
 }
