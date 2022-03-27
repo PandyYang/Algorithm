d9f64e56f63e74918cb42a8e957c5aa1ae51f211
PandyYang <yangpandy@gmail.com>
2022-03-27 23:10:01 +0800

    跳台阶

---
+++ src/main/java/com/pandy/base/dynamic/RunAway.java
@@ -0,0 +1,18 @@
+package com.pandy.base.dynamic;
+
+/**
+ * @author: Pandy
+ * @create: 2022/3/27
+ **/
+public class RunAway {
+    public int numWays(int n) {
+        int a = 1, b = 1, sum;
+        for(int i = 0; i < n; i++){
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+}
