f904bb5e4c84d6fb70596a0b98226128b082fedc
PandyYang <yangpandy@gmail.com>
2022-03-27 22:09:03 +0800

    斐波那契数列

---
+++ src/main/java/com/pandy/base/dynamic/Fib.java
@@ -0,0 +1,21 @@
+package com.pandy.base.dynamic;
+
+/**
+ * @author: Pandy
+ * @create: 2022/3/27
+ **/
+public class Fib {
+
+    private int mod = 1000000007;
+    public int fib(int n) {
+        int f0 = 0, f1 = 1;
+        if (n < 2) return n;
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            res = (f0 + f1) % mod;
+            f0 = f1 % mod;
+            f1 = res % mod;
+        }
+        return res;
+    }
+}
