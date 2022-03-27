413071ebb2c88982380fcbc720706a64d15e927c
PandyYang <yangpandy@gmail.com>
2022-03-27 20:36:50 +0800

    二分查找求数得平方根

---
+++ src/main/java/com/pandy/base/math/MySqrt.java
@@ -0,0 +1,24 @@
+package com.pandy.base.math;
+
+/**
+ * @author: Pandy
+ * @create: 2022/3/27
+ *
+ * 二分查找
+ * 求平方根
+ **/
+public class MySqrt {
+    public int mySqrt(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
