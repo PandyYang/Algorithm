7d449a56c04bef12ec78880d6e31663bae7eb798
PandyYang <yangpandy@gmail.com>
2022-03-29 11:59:49 +0800

    二叉树转换为双向链表 首先将二叉树中序遍历 然后组织链表

---
+++ src/main/java/com/pandy/base/tree/TreeToDoublyList.java
@@ -0,0 +1,44 @@
+package com.pandy.base.tree;
+
+import com.pandy.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Pandy
+ * @date 29/3/2022
+ *
+ * 将二叉搜索树转换为双向循环链表
+ */
+public class TreeToDoublyList {
+    private List<TreeNode> list = new ArrayList<>();
+
+    public TreeNode treeToDoublyList(TreeNode root) {
+        if (root == null) return null;
+        // 中序遍历
+        mid(root);
+        // 头结点
+        TreeNode p = list.get(0);
+        TreeNode head = p;
+        // 组装链表
+        for (int i = 1; i < list.size(); i++) {
+            TreeNode q = list.get(i);
+            p.right = q;
+            q.left = p;
+            p = q;
+        }
+
+        // 组装头结点
+        p.right = head;
+        head.left = p;
+        return head;
+    }
+
+    public void mid(TreeNode root) {
+        if (root == null) return;
+        mid(root.left);
+        list.add(root);
+        mid(root.right);
+    }
+}
