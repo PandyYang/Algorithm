be97d820c73cfa1d3d2f5cb02b6b9cc59d0cc747
PandyYang <yangpandy@gmail.com>
2022-03-31 12:18:44 +0800

    广度优先遍历求解岛屿的数量

---
+++ src/main/java/com/pandy/base/bfs/NumOfIslands.java
@@ -0,0 +1,61 @@
+package com.pandy.base.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Pandy
+ * @date 31/3/2022
+ *
+ *  岛屿的数量
+ *  广度优先遍历求解
+ */
+public class NumOfIslands {
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int res = 0;
+
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+                    res++;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    // 二维数组转换为1维数组
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.add((row - 1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+
+                        if (row + 1 < nr &&  grid[row+1][col] == '1') {
+                            neighbors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            neighbors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.add(row * nc + col + 1);
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
