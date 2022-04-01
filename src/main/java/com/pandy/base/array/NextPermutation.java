7ce7271135e9048c8dacd62384633329d310cbb1
PandyYang <yangpandy@gmail.com>
2022-04-01 11:43:55 +0800

    下一个排列 解题思路

--- src/main/java/com/pandy/base/array/NextPermutation.java
+++ src/main/java/com/pandy/base/array/NextPermutation.java
@@ -1,47 +1,32 @@
 package com.pandy.base.array;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author Pandy
  * @date 5/1/2022
  * 求下一个排列
+ *
+ * 下一个数比当前数更大，这样才满足"下一个排列的定义" 因此只需要将后面的大数与前面的小数交换
+ * 下一个数增加的幅度尽可能小
+ *      1.在尽可能靠右的低位进行交换，需要从后向前查找
+ *      2.将一个尽可能小的大数与前面的小数进行交换
+ *      3.将大数换到前面后，将大数后面的所有数重置为升序
+ *
  */
 public class NextPermutation {
-    //
-    public void next(int[] arr) {
-
-        int k = arr.length - 2;
-        while (k >= 0 && arr[k] > arr[k+1])
-            k--;
-        if (k < 0) {
-            Arrays.sort(arr);
-            return;
-        }
-        int i = k+2;
-        while (i < arr.length && arr[i] > arr[k])
-            i++;
-        // 找到降序的一个序列值
-        int temp = arr[k];
-        arr[k] = arr[k-1];
-        arr[k-1] = temp;
-
-        int start = k+1, end = arr.length - 1;
-        while (start < end) {
-            int reTemp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = reTemp;
-            start ++;
-            end --;
-        }
-    }
 
     public void nextPermutation(int[] nums) {
         int len = nums.length;
+        // 从后往前 （如果找不到 说明是最小序列 直接排序成最大的即可）
         for (int i = len - 1; i > 0 ; i--) {
+            // 查找一个升序的
             if (nums[i] > nums[i-1]) {
+
+                // 大数之后的所有数重置为升序 升序排列就是最小的排列
                 Arrays.sort(nums, i, len);
+
+                // 之后将第一个比i大的数字 进行交换
                 for (int j = i; j < len; j++) {
                     if (nums[j] > nums[i-1]) {
                         int temp = nums[i];
