99aca671ffcf86e84ebfe5cb5ecfd9ab60b8058d
PandyYang <yangpandy@gmail.com>
2022-04-04 22:16:32 +0800

    螺旋矩阵优化解法

--- src/main/java/com/pandy/base/array/chapter01/SpiralOrder.java
+++ src/main/java/com/pandy/base/array/chapter01/SpiralOrder.java
@@ -12,12 +12,17 @@ import java.util.List;
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> order = new ArrayList<Integer>();
+
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return order;
         }
+
         int rows = matrix.length, columns = matrix[0].length;
+
         int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+
         while (left <= right && top <= bottom) {
+
             for (int column = left; column <= right; column++) {
                 order.add(matrix[top][column]);
             }
@@ -38,5 +43,30 @@ class Solution {
             bottom--;
         }
         return order;
+    }
+
+    private static List<Integer> spiralOrder2(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        // 左 上从0开始
+        // 下 右 分别是row - 1 和 col - 1
+        int l = 0, t = 0, b = row - 1, r  = col - 1;
+
+        while (true) {
+            for (int i = l; i<= r; i++) res.add(matrix[t][i]);
+
+            if (++t > b) break;
+            for (int i = t; i <= b; i++) res.add(matrix[i][r]);
+
+            if (--r < l) break;
+            for (int i = r; i>= l; i--) res.add(matrix[b][i]);
+
+            if (--b < t) break;
+            for (int i = b; i>= t; i--) res.add(matrix[i][l]);
+            if (++l > r) break;;
+        }
+        return res;
     }
 }
