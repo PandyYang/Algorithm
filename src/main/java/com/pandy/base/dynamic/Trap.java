553beb46121e65765335564d85654629d091b33a
PandyYang <yangpandy@gmail.com>
2022-04-22 16:52:37 +0800

    接雨水

---
+++ src/main/java/com/pandy/base/dynamic/Trap.java
@@ -0,0 +1,29 @@
+package com.pandy.base.dynamic;
+
+/**
+ * @author Pandy
+ * @date 22/4/2022
+ *
+ * 接雨水
+ */
+public class Trap {
+
+    public int trap(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (leftMax < rightMax) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+}
