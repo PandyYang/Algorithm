2c1bea16db513aff432b9fdc2c589031811790d8
PandyYang <yangpandy@gmail.com>
2022-04-22 09:51:34 +0800

    几种排序

---
+++ src/main/java/com/pandy/base/sort/HeapSort.java
@@ -0,0 +1,37 @@
+package com.pandy.base.sort;
+
+/**
+ * @author Pandy
+ * @date 21/4/2022
+ */
+public class HeapSort {
+
+    void heapSort(int[] nums) {
+        int size = nums.length;
+        for (int i = size/2 - 1; i >=0 ; i--) {
+            adjust(nums, size, i);
+        }
+
+        for (int i = size -1; i >= 1 ; i--) {
+            int temp = nums[0];
+            nums[0] = nums[i];
+            nums[i] = temp;
+            adjust(nums, i, 0);
+        }
+    }
+
+    void adjust(int[] nums, int len, int index) {
+        int l = 2 * index + 1;
+        int r = 2 * index + 2;
+
+        int maxIndex = index;
+        if (l < len && nums[l] > nums[maxIndex]) maxIndex = l;
+        if (r < len && nums[r] > nums[maxIndex]) maxIndex = r;
+        if (maxIndex != index) {
+            int  temp = nums[maxIndex];
+            nums[maxIndex] = nums[index];
+            nums[index] = temp;
+            adjust(nums, len, maxIndex);
+        }
+    }
+}
--- src/main/java/com/pandy/base/sort/ShellSort.java
+++ src/main/java/com/pandy/base/sort/ShellSort.java
@@ -3,6 +3,10 @@ package com.pandy.base.sort;
 /**
  * @Author Pandy
  * @Date 2021/7/22 23:52
+ *
+ * 希尔排序 - 插入排序的改进版。为了减少数据的移动次数，在初始序列较大时取较大的步长，
+ * 通常取序列长度的一半，此时只有两个元素比较，交换一次；之后步长依次减半直至步长为1，
+ * 即为插入排序，由于此时序列已接近有序，故插入元素时数据移动的次数会相对较少，效率得到了提高。
  */
 public class ShellSort {
     public static void shell(int[] arr) {
@@ -18,6 +22,22 @@ public class ShellSort {
                 }
                 arr[j+step] = temp;
             }
+        }
+    }
+
+    void shellSort(int arr[]){
+        int d = arr.length >> 1;
+        while(d >= 1){
+            for(int i = d; i < arr.length; i++){
+                int rt = arr[i];
+                for(int j = i - d; j >= 0; j -= d){
+                    if(rt < arr[j]){
+                        arr[j + d] = arr[j];
+                        arr[j] = rt;
+                    }else break;
+                }
+            }
+            d >>= 1;
         }
     }
 }
