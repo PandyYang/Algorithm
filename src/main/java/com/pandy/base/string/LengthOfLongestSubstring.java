3ed9ab185473c7f227d6d252d252e724b8371384
PandyYang <yangpandy@gmail.com>
2022-04-24 22:33:18 +0800

    无重复字符的 最长字串 完善题意

--- src/main/java/com/pandy/base/string/LengthOfLongestSubstring.java
+++ src/main/java/com/pandy/base/string/LengthOfLongestSubstring.java
@@ -7,6 +7,9 @@ import java.util.*;
 /**
  * @author: Pandy
  * @create: 2022/3/29
+ *
+ * 无重复字符的最长子串
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
  **/
 public class LengthOfLongestSubstring {
     public static int length(String s) {
