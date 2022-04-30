999bfe5dda75294a21457293c7082fd1837168b7
PandyYang <yangpandy@gmail.com>
2022-04-30 09:46:05 +0800

    合并k个链表

---
+++ src/main/java/com/pandy/base/list/MergeKLists.java
@@ -0,0 +1,36 @@
+package com.pandy.base.list;
+
+import com.pandy.common.ListNode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author Pandy
+ * @date 22/4/2022
+ */
+public class MergeKLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((o1, o2) -> o1.val - o2.val);
+
+        for (ListNode list : lists) {
+            if (list == null) continue;
+            pq.add(list);
+        }
+
+        while (!pq.isEmpty()) {
+            ListNode poll = pq.poll();
+            curr.next = poll;
+            curr = curr.next;
+            if (poll.next != null) {
+                pq.add(poll.next);
+            }
+        }
+        return dummyHead.next;
+    }
+
+}
