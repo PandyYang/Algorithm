32c21c10d3d741749dca1765fa53bd8a59fc44d7
pandy <yangpandy@gmail.com>
2022-06-06 12:32:34 +0800

    组合问题

---
+++ src/main/java/com/pandy/base/dfs/Combine.java
@@ -0,0 +1,41 @@
+package com.pandy.base.dfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author Pandy
+ * @date 6/6/2022
+ *
+ * 组合
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ *
+ * 你可以按 任何顺序 返回答案。
+ */
+public class Combine {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (k <= 0 || n < k) return res;
+        Deque<Integer> path = new ArrayDeque<>();
+
+        dfs(n, k, 1, path, res);
+        return res;
+    }
+
+    private void dfs(int n, int k, int begin, Deque<Integer> path, List<List<Integer>> res) {
+        // 递归终止条件是 path的长度等于k
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i <= n; i++) {
+            path.addLast(i);
+            dfs(n, k, i+1, path, res);
+            path.removeLast();
+        }
+    }
+}
