8a6fa179fc986f2529bc1f48517c624a549f71e0
PandyYang <yangpandy@gmail.com>
2022-06-06 20:05:21 +0800

    最长无重复字符子串

---
+++ src/main/java/com/pandy/base/map/LongestNoRepeatSubString.java
@@ -0,0 +1,33 @@
+package com.pandy.base.map;
+
+import java.util.HashMap;
+
+/**
+ * @author: Pandy
+ * @create: 2022/6/6
+ *
+ * 给定一个字符串，请你找出其中不含有重复字符的最长子串的长度
+ *
+ * 用 Map 记录字符所在位置，当遇到重复字符时，移动 start 指针
+ * 替换 Map 中下标，并计算子串长度
+ **/
+public class LongestNoRepeatSubString {
+
+    public int longest(String str) {
+        if (str == null || str.length() == 0) return 0;
+
+        HashMap<Character, Integer> temp = new HashMap<>();
+        char[] chars = str.toCharArray();
+
+        int res = 0, start = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (temp.containsKey(chars[i])) {
+                start = Math.max(temp.put(chars[i], i) + 1, start);
+            }
+            temp.put(chars[i], i);
+            res = Math.max(res, i - start + 1);
+        }
+        return res;
+    }
+}
