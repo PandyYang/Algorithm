59e969f7ae4a9a1915d1c3da2ab6a147a3e361d9
pandy <yangpandy@gmail.com>
2022-06-13 14:33:17 +0800

    更新背包问题

--- src/main/java/com/pandy/base/dynamic/PackageProblem.java
+++ src/main/java/com/pandy/base/dynamic/PackageProblem.java
@@ -3,7 +3,7 @@ package com.pandy.base.dynamic;
 /**
  * @author Pandy
  * @date 23/2/2022
- * 
+ *
  * 描述：
  * 有N件物品和一个重量为V的包，第i件物品的重量为w[i]，对应的价值为v[i]，求
  * 将哪些物品放入背包可以使这些物品的重量总和不能超过背包容量，且价值总和最大。
@@ -53,6 +53,27 @@ public class PackageProblem {
         }
         int v2 = pro2(i - 1, j);
         return Math.max(v1, v2);
+    }
+
+    public static int Bagging(int[] c, int[] v, int order) {
+        //这些情况背包问题都是不成立的
+        if (c == null || v == null || c.length != v.length) {
+            return -1;
+        }
+        int n = c.length;
+        int[][] dp = new int[n + 1][order + 1];
+        //因为最后一行全为0 java数组默认都是0 从倒数第二行开始计算值
+        for (int index = n - 1; index >= 0; index--) {
+            for (int col = 0; col <= order; col++) {
+                int p1 = dp[index + 1][col];
+                int p2 = 0;
+                if (col - c[index] >= 0) {
+                    p2 += v[index] + dp[index + 1][col - c[index]];
+                }
+                dp[index][col] = Math.max(p1, p2);
+            }
+        }
+        return dp[0][order];
     }
 
     public static void main(String[] args) {
