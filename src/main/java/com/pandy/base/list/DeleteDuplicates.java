0aece8e7c214cf13002f02033206f9fb8ca0271e
pandy <yangpandy@gmail.com>
2022-06-16 00:28:15 +0800

    删除链表中重复的元素

---
+++ src/main/java/com/pandy/base/list/DeleteDuplicates.java
@@ -0,0 +1,25 @@
+package com.pandy.base.list;
+
+import com.pandy.common.ListNode;
+
+/**
+ * @author Pandy
+ * @date 16/6/2022
+ *
+ * 删除链表中重复的元素
+ */
+public class DeleteDuplicates {
+
+    public ListNode deleteDuplicates (ListNode head) {
+        // write code here
+        ListNode cur = head;
+        while(cur != null) {
+            while(cur.next != null && cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
+}
