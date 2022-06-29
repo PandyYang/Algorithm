f189e502284e8472ba8149f144b98e6bece93bb8
PandyYang <yangpandy@gmail.com>
2022-06-29 11:35:56 +0800

    判断是否为字母异位词

---
+++ src/main/java/com/pandy/base/array/IsAnagram.java
@@ -0,0 +1,22 @@
+package com.pandy.base.array;
+
+import java.util.Arrays;
+
+/**
+ * @author: Pandy
+ * @create: 2022/6/29
+ *
+ * 判断是否为字母异位词
+ **/
+public class IsAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
