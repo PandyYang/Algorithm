572937e70c2e8ea39ef94e99fa2963dfa05a69a0
PandyYang <yangpandy@gmail.com>
2022-06-29 17:06:52 +0800

    分割等和子集

---
+++ src/main/java/com/pandy/base/array/ReconstructQueue.java
@@ -0,0 +1,33 @@
+package com.pandy.base.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author: Pandy
+ * @create: 2022/6/29
+ **/
+public class ReconstructQueue {
+
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                } else {
+                    return o2[0] - o1[0];
+                }
+            }
+        });
+
+        List<int[]> list = new ArrayList<>();
+
+        for (int[] each : people) {
+            list.add(each[1],each);
+        }
+        return list.toArray(new int[list.size()][2]);
+    }
+}
---
+++ src/main/java/com/pandy/base/stack_queue/DecodeString.java
@@ -0,0 +1,44 @@
+package com.pandy.base.stack_queue;
+
+import java.util.Stack;
+
+/**
+ * @author: Pandy
+ * @create: 2022/6/29
+ **/
+public class DecodeString {
+    public String decodeString(String s) {
+
+        StringBuffer ans = new StringBuffer();
+
+        Stack<Integer> multiStack = new Stack<>();
+        Stack<StringBuffer> ansStack = new Stack<>();
+
+        int multi = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) multi = multi * 10 + c - '0';
+            else if (c =='[') {
+                ansStack.add(ans);
+                multiStack.add(multi);
+                ans = new StringBuffer();
+                multi = 0;
+            } else if (Character.isAlphabetic(c)) {
+                ans.append(c);
+            } else {
+                StringBuffer ansTmp = ansStack.pop();
+                int tmp = multiStack.pop();
+                for (int i = 0; i < tmp; i++) {
+                    ansTmp.append(ans);
+                }
+                ans = ansTmp;
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        DecodeString decodeString = new DecodeString();
+        String s = decodeString.decodeString("3[a2[c]]");
+        System.out.println("s = " + s);
+    }
+}
