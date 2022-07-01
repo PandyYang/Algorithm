91c10efdf8baaf17e40dfe09892254789499cfca
PandyYang <yangpandy@gmail.com>
2022-07-01 15:37:57 +0800

    update code

--- src/main/java/com/pandy/company/bytedance/Solve.java
+++ src/main/java/com/pandy/company/bytedance/Solve.java
@@ -3,27 +3,36 @@ package com.pandy.company.bytedance;
 /**
  * @author: Pandy
  * @create: 2022/7/1
+ *
+ * 被围绕的区域
+ *
+ * 给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
  **/
 public class Solve {
     public void solve(char[][] board) {
         if (board == null || board.length == 0) return;
         int m = board.length;
         int n = board[0].length;
+
+        // 遍历左右边界
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                // 从边缘o开始搜索
-                boolean isEdge = i == 0 || j == 0 || i == m - 1 || j == n - 1;
-                if (isEdge && board[i][j] == 'O') {
-                    dfs(board, i, j);
-                }
-            }
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        // 遍历上下边界
+        for (int i = 0; i < n; i++) {
+            dfs(board, 0, i);
+            dfs(board, m-1, i);
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                // 如果该字母没有被标记过，则该字母为被字母X包围的字母O，我们将其修改为字母X
                 if (board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
+                // 如果该字母被标记过，则该字母为没有被字母X包围的字母O，我们将其还原为字母O；
                 if (board[i][j] == '#') {
                     board[i][j] = 'O';
                 }
@@ -32,8 +41,8 @@ public class Solve {
     }
 
     public void dfs(char[][] board, int i, int j) {
+        // board[i][j] == '#' 说明已经搜索过了.
         if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == 'X' || board[i][j] == '#') {
-            // board[i][j] == '#' 说明已经搜索过了.
             return;
         }
         board[i][j] = '#';
