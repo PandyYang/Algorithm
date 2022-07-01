ba9ff869473833df05b0a63cd37f64afe8f8acde
PandyYang <yangpandy@gmail.com>
2022-07-01 20:28:57 +0800

    z字型变换

---
+++ src/main/java/com/pandy/company/bytedance/Zconvert.java
@@ -0,0 +1,32 @@
+package com.pandy.company.bytedance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Zconvert {
+    public String convert(String s, int numRows) {
+        if (numRows < 2) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int i = 0, flag = -1;
+
+        for (char c : s.toCharArray()) {
+            rows.get(i).append(c);
+
+            //  i为0或最后一个 分别加1 或者减去 1 z字形振动
+            if (i==0 || i == numRows - 1) flag = -flag;
+            i += flag;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rows) {
+            res.append(row);
+        }
+
+        return res.toString();
+    }
+}
