ae9c0ae0bf69c2f1121617ef7b54229df8db509e
PandyYang <yangpandy@gmail.com>
2022-07-01 17:30:35 +0800

    update code

---
+++ src/main/java/com/pandy/company/bytedance/GetIntersectionNode.java
@@ -0,0 +1,29 @@
+package com.pandy.company.bytedance;
+
+import com.pandy.common.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author: Pandy
+ * @create: 2022/7/1
+ **/
+public class GetIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> listNodes = new HashSet<>();
+        while (headA != null) {
+            listNodes.add(headA);
+            headA = headA.next;
+        }
+
+        while (headB != null) {
+            if (listNodes.contains(headB)) {
+                return headB;
+            }
+
+            headB = headB.next;
+        }
+        return null;
+    }
+}
