84ca293026d60d73b9d989b291ec9e285ed5aa21
PandyYang <yangpandy@gmail.com>
2022-07-03 14:50:16 +0800

    update dp solvation

--- src/main/java/com/pandy/company/bytedance/MaximalSquare.java
+++ src/main/java/com/pandy/company/bytedance/MaximalSquare.java
@@ -21,6 +21,32 @@ package com.pandy.company.bytedance;
  **/
 public class MaximalSquare {
 
+
+    /**
+     * 动态规划求解
+     * @param matrix
+     * @return
+     */
+    public int maximumSquare2(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return maxSide;
+        int row = matrix.length, col = matrix[0].length;
+        int[][] dp = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                }
+                maxSide = Math.max(maxSide, dp[i][j]);
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+
     public int maximumSquare(char[][] matrix) {
         int maxSide = 0;
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return maxSide;
