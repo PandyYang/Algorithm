a0dd230f87850ec3e4e8447c8e37040185fa1b3d
PandyYang <yangpandy@gmail.com>
2022-07-03 21:24:32 +0800

    update code

---
+++ src/main/java/com/pandy/company/bytedance/LargestNumber.java
@@ -0,0 +1,30 @@
+package com.pandy.company.bytedance;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author: Pandy
+ * @create: 2022/7/3
+ *
+ * 最大数
+ **/
+public class LargestNumber {
+
+    public String largestNumber(int[] nums) {
+        PriorityQueue<String> heap = new PriorityQueue<>(
+                (x, y) -> (y + x).compareTo(x + y)
+        );
+
+        for (int num : nums) {
+            heap.offer(String.valueOf(num));
+        }
+
+        String res = "";
+
+        while (heap.size() > 0) res += heap.poll();
+
+        if (res.charAt(0) == '0') return "0";
+
+        return res;
+    }
+}
