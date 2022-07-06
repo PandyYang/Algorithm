41694219744a7b072fd26720be5c3b7219d01bff
pandy <yangpandy@gmail.com>
2022-07-06 13:59:03 +0800

    罗马数字转换整数，注意左右递增还是递减

---
+++ src/main/java/com/pandy/base/simulation/RomanToInt.java
@@ -0,0 +1,37 @@
+package com.pandy.base.simulation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Pandy
+ * @date 6/7/2022
+ *
+ * 罗马数字转整数
+ */
+public class RomanToInt {
+
+    Map<Character, Integer> symbolValues = new HashMap<Character, Integer>(){{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public int romanToInt(String s) {
+        int sum = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i+1))) {
+                sum -= value;
+            } else {
+                sum += value;
+            }
+        }
+        return sum;
+    }
+}
