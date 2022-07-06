5d8a53263273fe60adeb166c8b11a19d07d5bbef
pandy <yangpandy@gmail.com>
2022-07-06 17:08:53 +0800

    旋转数组的搜索 二分搜索的细节拉满

--- src/main/java/com/pandy/base/array/package02/Search.java
+++ src/main/java/com/pandy/base/array/package02/Search.java
@@ -1,50 +1,25 @@
 package com.pandy.base.array.package02;
 
-/**
- * @author: Pandy
- * @create: 2022/3/24
- * <p>
- * 整数数组 nums 按升序排列，数组中的值 互不相同 。
- * <p>
- * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
- * 使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]
- * （下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为[4,5,6,7,0,1,2] 。
- * <p>
- * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
- **/
-public class Search {
-
-    public int search(int[] nums, int target) {
-
-        int firstNum = nums[0];
-        int left = 0;
-        int right = nums.length - 1;
-
-        if (target < firstNum) {
-            //目标中只可能出现在后半部分
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                if (nums[mid] == target) {
-                    return mid;
-                } else if (nums[mid] > target && nums[mid] < firstNum) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+class Search {
+    public boolean search(int[] nums, int target) {
+        if (nums.length == 0) return false;
+        if (nums.length == 1) return nums[0] == target;
+        int left = 0, right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[left] < nums[mid]){ //如果左侧有序
+                if (nums[left] <= target && target < nums[mid]) right = mid - 1;
+                else left = mid + 1;
+            }else if (nums[right] > nums[mid]){ //如果右侧有序
+                if (nums[mid] < target && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
             }
-        } else {
-            //目标值只可能出现在前半部分
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                if (nums[mid] == target) {
-                    return mid;
-                } else if (nums[mid] < target && nums[mid] >= firstNum) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+            else{ //如果与mid值相等，两边向中间压缩
+                while(left <= right && nums[left] == nums[mid]) left++;
+                while(left <= right && nums[right] == nums[mid]) right--;
             }
         }
-        return -1;
+        return false;
     }
 }
