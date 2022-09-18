2c4e2d47707e39b59c78c44432942bc98db28bce
PandyYang <yangpandy@gmail.com>
2022-09-18 23:30:02 +0800

    删除数组重复项，总是用i记录无重复的最长位置。

--- src/main/java/com/pandy/base/array/RemoveDuplicates.java
+++ src/main/java/com/pandy/base/array/RemoveDuplicates.java
@@ -2,28 +2,27 @@ package com.pandy.base.array;
 
 /**
  * 删除有序数组的重复项
+ * 要求原地删除
  */
 
 class RemoveDuplicates {
-    public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        if (n <= 2) {
-            return n;
-        }
-        int slow = 2, fast = 2;
-        while (fast < n) {
-            if (nums[slow - 2] != nums[fast]) {
-                nums[slow] = nums[fast];
-                ++slow;
+
+    public int remove(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i]  = nums[j];
             }
-            ++fast;
         }
-        return slow;
+        return i + 1;
     }
 
     public static void main(String[] args) {
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        int i = removeDuplicates.removeDuplicates(new int[]{1, 1, 1, 2, 2, 2, 3, 3, 4});
-        System.out.println(i);
+        int i2 = removeDuplicates.remove(new int[]{1, 1, 1, 2, 2, 2, 3, 3, 4});
+        System.out.println(i2);
     }
 }
