673709d253f709fc8dcf73a11515563c6a0c38cb
Pandy <pan19970218>
2023-08-04 16:09:34 +0800

    update: 求二叉树第k小的节点

--- src/main/java/com/pandy/base/tree/kthSmallest.java
+++ src/main/java/com/pandy/base/tree/kthSmallest.java
@@ -1,34 +1,31 @@
 package com.pandy.base.tree;
 
 import com.pandy.common.TreeNode;
+import com.sun.source.tree.Tree;
 
 /**
  * @author Pandy
  * @date 19/4/2022
  * <p>
  * 二叉搜索树 找到树中第k小的节点
+ * 使用中序遍历，中序遍历从小到大，第k小，就是遍历到的第k个值。
  */
 public class kthSmallest {
+    int res = 0;
+    int rank = 0;
 
-    public int kth(TreeNode root, int k) {
-        int leftN = findChild(root.left);
-        if (leftN + 1 == k) {
-            return root.val;
-        } else if (k <= leftN) {
-            return kth(root.left, k);
-        } else {
-            return kth(root.right, k - leftN - 1);
+    void traverse(TreeNode root, int k) {
+        if(root == null) {
+            return;
         }
-    }
+        traverse(root.left, k);
 
-    /**
-     * 查找子节点的个数
-     *
-     * @param root
-     * @return
-     */
-    public int findChild(TreeNode root) {
-        if (root == null) return 0;
-        return findChild(root.left) + findChild(root.right) + 1;
+        rank ++;
+        if(rank == k) {
+            res = root.val;
+            return;
+        }
+
+        traverse(root.right, k);
     }
 }
