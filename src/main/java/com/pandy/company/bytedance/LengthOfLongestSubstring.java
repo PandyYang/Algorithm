8ffb03b8ed6c52dcab8f9b5bbf877aac751b6ab9
PandyYang <yangpandy@gmail.com>
2023-11-21 10:26:25 +0800

    update: 无重复的最长字串。

--- src/main/java/com/pandy/company/bytedance/LengthOfLongestSubstring.java
+++ src/main/java/com/pandy/company/bytedance/LengthOfLongestSubstring.java
@@ -1,5 +1,6 @@
 package com.pandy.company.bytedance;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,21 +13,16 @@ import java.util.Set;
  * 使用map作为滑动窗口，每次遇见重复的数字，更新下当前位置和开始扫描位置的距离，记录最大值。
  **/
 public class LengthOfLongestSubstring {
-
-    public int lengthOfLongestSubstring(String s) {
-        int max = 0;
+    public int lengthOfLongestSubstring2(String s) {
+        HashMap<Character, Integer> map= new HashMap<>();
+        int max = 0, start = 0;
         for (int i = 0; i < s.length(); i++) {
-            Set<Character> set = new HashSet<>();
-            for (int j = i; j < s.length(); j++) {
-                if (!set.contains(s.charAt(j))) {
-                    set.add(s.charAt(j));
-                } else {
-                    break;
-                }
-                if (max < set.size()) {
-                    max = set.size();
-                }
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                start = Math.max(map.get(c) + 1, start); // 第一次出现重复元素的位置就是窗口的开始位置，也就是最左边的位置
             }
+            max = Math.max(max, i - start + 1); //  i - start + 1 是当前窗口大小
+            map.put(c, i); // 记录当前元素的位置
         }
         return max;
     }
