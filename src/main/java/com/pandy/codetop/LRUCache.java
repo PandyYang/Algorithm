2b7cdd41f7bd45f260ddc8ca7493af20e6679cc8
PandyYang <yangpandy@gmail.com>
2024-01-06 11:47:49 +0800

    lru缓存

---
+++ src/main/java/com/pandy/codetop/LRUCache.java
@@ -0,0 +1,41 @@
+package com.pandy.codetop;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    private int cap;
+    private Map<Integer, Integer> map = new LinkedHashMap<>();
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+    }
+
+    public int get(int key) {
+        if (map.keySet().contains(key)) {
+            Integer v = map.get(key);
+            map.remove(v);
+            map.put(key, v);
+            return v;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (map.keySet().contains(key)) {
+            map.remove(key);
+            map.put(key, value);
+            // 到达容量 要清空最旧的元素
+        }else if (map.size() == cap) {
+            Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+            if (iterator.hasNext()) {
+                iterator.next();
+                iterator.remove();
+                map.put(key,value);
+            }
+        }
+    }
+}
