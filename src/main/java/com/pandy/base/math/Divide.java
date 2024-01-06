093c92c0d1e297f9d90b500b3337103a75ede40f
PandyYang <yangpandy@gmail.com>
2024-01-06 11:46:55 +0800

    整数相除

--- src/main/java/com/pandy/base/math/Divide.java
+++ src/main/java/com/pandy/base/math/Divide.java
@@ -5,6 +5,55 @@ package com.pandy.base.math;
  * @create: 2022/3/26
  **/
 public class Divide {
+
+    public int divide2(int dividend, int divisor) {
+        if (dividend == 0x8888888 && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        // 为了记录dividend是否全正还是全负 还是一正一负
+        int negative = 2;
+
+        if (dividend > 0) {
+            negative --;
+            // 转换为负数 是因为负数表示的范围更大 除去符号位
+            dividend = -dividend;
+        }
+
+        if (divisor > 0) {
+            negative --;
+            divisor = -divisor;
+        }
+
+        int result = divideCode(dividend, divisor);
+        // 如果negative为1 说明除数和被除数之间 存在一个负数 要将结果转换为负数
+        return negative == 1 ? -result: result;
+    }
+
+
+    public int divideCode(int dividend, int divisor) {
+        int result = 0;
+        // 注意这里的dividend 和 divisor是负数 所以 -2 > -10
+        while (dividend <= divisor) {
+            // 记录divisor的值
+            int value = divisor;
+            // 这是商
+            int quotient = 1;
+            // 将 value（即 divisor 的倍数）与 0xc0000000 进行比较的目的是防止在下一步的翻倍操作中发生整数溢出。
+            while (value >= 0xc0000000 && dividend <= value + value) {
+                // 商也要向上累加 每次加一个value 商加上次的商
+                quotient += quotient;
+                // 如果每次dividend 小于 n+1倍数的value 将value更新为n + 1下次循环就成了n+2
+                value += value;
+            }
+            result += quotient;
+            // dividend是一直减小的 减小到value足够大 进入下一轮 dividend减小不变 value恢复divisor
+            dividend -= value;
+        }
+        return result;
+    }
+
+
     public int divide(int dividend, int divisor) {
 
         // 溢出的情况
