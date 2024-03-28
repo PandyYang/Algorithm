e38c0f70fa53830bb3c8079a9c01dbdf3839b53a
PandyYang <yangpandy@gmail.com>
2024-03-28 17:24:17 +0800

    update

--- src/main/java/com/pandy/base/tree/TreeToDoublyList.java
+++ src/main/java/com/pandy/base/tree/TreeToDoublyList.java
@@ -1,5 +1,6 @@
 package com.pandy.base.tree;
 
+import com.pandy.common.Node;
 import com.pandy.common.TreeNode;
 
 import java.util.ArrayList;
@@ -40,5 +41,22 @@ public class TreeToDoublyList {
         mid(root.left);
         list.add(root);
         mid(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        TreeNode root2 = new TreeNode(2);
+        TreeNode root3 = new TreeNode(3);
+        TreeNode root4 = new TreeNode(4);
+        TreeNode root5 = new TreeNode(5);
+        root4.val = 4;
+        root4.left = root2;
+        root4.right = root5;
+        root2.left = root1;
+        root2.right = root3;
+
+        TreeToDoublyList solution = new TreeToDoublyList();
+        TreeNode node = solution.treeToDoublyList(root4);
+        System.out.println("node = " + node);
     }
 }
